17eeb5c0e4642eb6325b54504fab4e6b268b276b
Sumeet Bajaj <30503794+sbajaj5@users.noreply.github.com>
2018-11-23 12:41:55 -0500

    Adding CSS and Updating Photo Component

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,8 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class AddPhoto extends Component {
   render() {
-    return <h1>This is the page where we will add photo</h1>
+    return (
+      <div>
+        <h1>PhotoWall</h1>
+        <div className = "form">
+          <form>
+            <input type="text" placeholder="Link" />
+            <input type="text" placeholder="Description" />
+            <button>Post</button>
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
